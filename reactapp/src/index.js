bf2eb0ebad701c587e0b0ab9f9ca62d24ed9c766
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-03 14:17:34 +0530

    refactoring fBC to CBC

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
-    return (
-        <div>Hello        </div>
-    )
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        )
+        return (
+            <div>Latitude :         </div>
+        )
+    }
 }
 
 
