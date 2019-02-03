0ab0b637d606c892dfd0ab91c6558d95530257ee
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-03 13:14:31 +0530

    Setup Done for Section 4 Class Based Components

---
+++ reactapp/src/SeasonDisplay.js
@@ -0,0 +1,12 @@
+import  React from 'react'
+
+
+const  SeasonDisplay = () =>
+{
+    return (
+        <div>Hi Season!</div>
+    )
+}
+
+
+export default SeasonDisplay;
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,52 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'Faker';
-import CommentDetail from './CommentDetail'
-import ApprovalCard from './ApprovalCard'
-
 
 
 
 const App = () => {
 
     return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <CommentDetail author="sam"
-                    timeAgo="Today at 4:45 PM"
-                    content="blogPost1"
-                    avatar={faker.Image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="alex"
-                    timeAgo="Today at 6:42 PM"
-                    content="blogPost2"
-                    avatar={faker.Image.avatar()} />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="jane"
-                    timeAgo="Yesterday at 1:10 AM"
-                    content="blogPost3"
-                    avatar={faker.Image.avatar()} />
-            </ApprovalCard>
-            
-            <Message header= "Changes in society" info="something about society"/>
-
-
-        </div>)
+    <div>Hello        </div>
+)
 }
 
-const Message = (props) =>
-{
-    return(
-        <div className="ui message">
-        <div className="header">{props.header}</div>
-        <p>{props.info}</p>
-       
-        </div>
-    )
-}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
