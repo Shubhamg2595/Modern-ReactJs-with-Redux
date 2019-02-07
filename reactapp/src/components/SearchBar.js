c3203671b5c667304889aaf9fd4e0e75c0d9e5c7
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-07 20:43:41 +0530

    Search Bar Updated

--- reactapp/src/components/SearchBar.js
+++ reactapp/src/components/SearchBar.js
@@ -11,8 +11,8 @@ class SearchBar extends React.Component {
                     <div className='field'>
                         <label>Image Search</label>
                         <input type='text'
-                        value={this.state.term}
-                        onChange={(event) => this.setState({ term: event.target.value })} />
+                            value={this.state.term}
+                            onChange={(event) => this.setState({ term: event.target.value.toUpperCase() })} />
                     </div>
                 </form>
             </div>
