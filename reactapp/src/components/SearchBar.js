2fb2cf3fb66e88bb1c003cbaa6dd9ce973873fe5
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-07 20:06:58 +0530

    Code changed from Uncontrolled comp to Controlled comp

--- reactapp/src/components/SearchBar.js
+++ reactapp/src/components/SearchBar.js
@@ -2,22 +2,18 @@ import React from 'react'
 
 class SearchBar extends React.Component {
 
-    onInputChange(event)
-    {
-        // console.log(event)
-        console.log(event.target.value)
-
-    }
+    state = { term: '' }
 
     render() {
         return (
             <div className='ui segment'>
                 <form className='ui form'>
-                <div className='field'>
-                <label>Image Search</label>
-                <input type='text' 
-                onChange = {(event) => console.log(event.target.value)} />
-                </div>
+                    <div className='field'>
+                        <label>Image Search</label>
+                        <input type='text'
+                        value={this.state.term}
+                        onChange={(event) => this.setState({ term: event.target.value })} />
+                    </div>
                 </form>
             </div>
         )
