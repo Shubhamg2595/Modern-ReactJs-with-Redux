2a1465f99c95f1f1336448b323a5e6fec6884a55
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-07 20:00:35 +0530

    Event handlers Introduced

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class App extends React.Component {
 
-   
-    render() {
-        return(
-            <div>
-              <h1>  Hello</h1>
-            </div>
-        )
-    }
-}
-
-
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
