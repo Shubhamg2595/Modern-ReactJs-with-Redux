d26062058ae5f1ac8c1267697e1188e694bf66d0
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-10 17:06:47 +0530

    Section 8 setup done

---
+++ reactapp/src/Notes/8 Making API requests.js
@@ -0,0 +1,24 @@
+{/*
+
+    1. AXIOS VS FETCH
+ 
+
+    FETCH is not actually a library but  is used frequently with
+    react applications for managing network requessts and
+    fetching some amount of data.
+    fetch is actually a function supported by all the browsers
+    However fetch is a far mor basic and lower level
+    function used to fetchh data.
+    If wee use fetch,we will hav to write a lot of code
+    that is already written in axios packages
+
+
+
+    Axios is a standalone third party package that can
+    be installed using npm.
+    easier to use
+    shorter line of code
+
+    
+    
+*/}
