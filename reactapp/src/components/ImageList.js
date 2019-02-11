d0d4c8f7ee662ca9775aae865b233dfcd6bdbccb
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-11 15:04:39 +0530

    Images Displayed SuccessFully

--- reactapp/src/components/ImageList.js
+++ reactapp/src/components/ImageList.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const ImageList = (props) =>
 {
-    console.log(props.images)
-    return( 
-        
-        <div>ImageList</div>
-    )
+   
+    const images = props.images.map((image)=>{
+        return (<img key={image.id} src={image.urls.regular} alt={image.description}/>)
+    })
+    
+    return <div >{images}</div>
 }
 
 export default ImageList;
