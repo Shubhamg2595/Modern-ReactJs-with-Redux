5e3e741b4389663a86347a7ac89fab4f7ae55efb
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-11 22:54:23 +0530

    Updating state with fetched data

--- reactapp/src/components/App.js
+++ reactapp/src/components/App.js
@@ -6,12 +6,14 @@ import youtube from '../api/youtube'
 class App extends React.Component
 {
     state={videos : []}
-    onTermSubmit =  (term) =>{
-      youtube.get('./search',{
+    onTermSubmit = async (term) =>{
+      const response = await youtube.get('./search',{
             params :{
                 q:term
             }
         })
+        
+        this.setState({videos:response.data.items})
        
     }
 
@@ -22,6 +24,7 @@ class App extends React.Component
             return(
                 <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                I have {this.state.videos.length} videos
                 </div>
             )
         }
