21665f8d4c6b9af9895dd6d0501e4289a7377027
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-11 22:48:43 +0530

    Putting together SearchBar and Youtube API

--- reactapp/src/components/App.js
+++ reactapp/src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import SearchBar, { } from './SearchBar';
+import SearchBar from './SearchBar';
+
+import youtube from '../api/youtube'
 
 class App extends React.Component
 {
+    state={videos : []}
+    onTermSubmit =  (term) =>{
+      youtube.get('./search',{
+            params :{
+                q:term
+            }
+        })
+       
+    }
+
+
+
     render()
         {
             return(
-                <div className="ui container"><SearchBar /></div>
+                <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                </div>
             )
         }
 }
