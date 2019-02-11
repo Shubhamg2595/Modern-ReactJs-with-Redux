0234db0e937535be0d13c234cff37ff66757bf36
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-11 17:28:32 +0530

    Use of Dynamic Spans For Styling

--- reactapp/src/components/ImageCard.js
+++ reactapp/src/components/ImageCard.js
@@ -4,25 +4,31 @@ class ImageCard extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state =
+         {
+              spans: 0 
+         } ;
         this.imageRef = React.createRef();
     }
 
-    componentDidMount()
-    {
-       this.imageRef.current.addEventListener('load',this.setSpans);
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
     }
 
-    setSpans = () =>
-    {
-        console.log(this.imageRef.current.clientHeight)
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10)
+
+        this.setState({ spans });
     }
 
- 
+
     render() {
         const { description, urls } = this.props.image;
         return (
-            <div>
-                <img ref={this.imageRef} 
+            <div style={{gridRowEnd : `span ${this.state.spans}`}}>
+                <img ref={this.imageRef}
                     alt={description}
                     src={urls.regular} />
             </div>
