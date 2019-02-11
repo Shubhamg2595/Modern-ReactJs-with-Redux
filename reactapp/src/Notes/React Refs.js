bc325c158573a7ac8ad34754890dac7fcc2c463d
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-11 16:37:42 +0530

    Understood use of DOM Refs

---
+++ reactapp/src/Notes/React Refs.js
@@ -0,0 +1,12 @@
+{/*
+React Refs
+
+a. Gives access to a Single DOM element
+
+b. We create refs in the constructor,
+    assign them to instance variables,
+    and then pass to a particular JSX 
+    element as props.
+    
+
+*/}
