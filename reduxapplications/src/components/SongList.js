73743e99cbd8e04847b792266435eac0c62b533d
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-13 15:32:18 +0530

    connect and MapStateToProps Explained

--- reduxapplications/src/components/SongList.js
+++ reduxapplications/src/components/SongList.js
@@ -12,4 +12,41 @@ class SongList extends Component
 
 }
 
-export default connect()(SongList);
+const mapStateToProps =(state) =>
+{
+    
+    return {songs : state.songs}
+}
+
+export default connect(mapStateToProps)(SongList);
+
+/*
+1.
+function connect()
+{
+    return function()
+    {
+        return 'hello'
+    }
+}
+
+connect() //no output
+connect()() //hello
+
+2.
+mapStateToProps : is also a convention not a fix method,
+so we can simply make it as getMyState
+
+this method conveys that we are going to take our state 
+object i.e all of the data that's inside of the redux store
+and we are going to run some calculation on it
+or some computation that's going to cause that data
+to eventually show up as props inside of our component
+
+3. in above mapStateToProps methods,
+
+line : {songs : state.songs}
+is actually creating a props for our songlish component
+as we already have all the app data present in State which
+we are getting from connect that is somehow connected to Provider
+*/
