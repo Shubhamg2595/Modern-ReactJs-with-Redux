6b58251b9bc339a18b86fc8a4a43d45706ec79e4
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-15 14:53:21 +0530

    Setup Done for Redux Thunk and Middleware

--- reduxapplications/src/index.js
+++ reduxapplications/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import App from './components/App';
-import reducers from './reducers'
 
+const App = () =>
+{
+    return(
+        <div>Hello</div>
+    )
+}
 
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
+        <App />,document.querySelector('#root')
 )
