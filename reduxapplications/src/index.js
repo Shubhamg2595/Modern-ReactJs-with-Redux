8b227ce0fde55dad48d1b9fb2292274c4ef6346a
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-15 15:24:02 +0530

    Tricking Redux with Dummy Reducers

--- reduxapplications/src/index.js
+++ reduxapplications/src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () =>
-{
-    return(
-        <div>Hello</div>
-    )
-}
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App'
+import reducers from './reducers'
 
 
 ReactDOM.render(
-        <App />,document.querySelector('#root')
+    <Provider store={createStore(reducers)}>
+    <App />
+    </Provider>
+    , document.querySelector('#root')
 )
+
+
+{/*
+App Goals
+
+1. understand the purpose of reducers
+2.understand making api requests with Redux
+3.understand purpose of redux Thunk
+
+Informatio
+
+1. Redux : the redux library
+2. react-redux : integration layer between react and redux
+3. axios: Helps us make network requests 
+4. redux-thunk :
+ Middleware to help us make requests in
+ a redux application 
+
+
+
+
+*/}
