8440b013a77ae310e16f5f731b43d0d799d3414b
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-17 13:18:20 +0530

    Redux Thunk Shorthand Syntax

--- reduxapplications/src/actions/index.js
+++ reduxapplications/src/actions/index.js
@@ -1,14 +1,13 @@
 import jsonPlaceholder from '../api/jsonPlaceholder'
 
-export const fetchPosts =  () =>
-{
-    return async function(dispatch,getState)
-    {
+
+export const fetchPosts =  () => async dispatch => 
+        {
         const response = await jsonPlaceholder.get('./posts')
         
         dispatch({type:'FETCH_POSTS',payload:response})
     }
-}
+
 /*
 
 MIDDLEWARES IN REDUX
