804f84d98094684b4cab653dbe39bc710a08e953
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-17 18:58:46 +0530

    chaining

--- reduxapplications/src/actions/index.js
+++ reduxapplications/src/actions/index.js
@@ -6,10 +6,16 @@ export const fetchPostsAndUsers = () => async (dispatch,getState) =>
 {
     await dispatch(fetchPosts())
     
-    const userIds=_.uniq(_.map(getState().posts,'userId'))
-    // console.log(userIds)
-    userIds.forEach(id=>dispatch(fetchUser(id)))
+    // const userIds=_.uniq(_.map(getState().posts,'userId'))
+    // userIds.forEach(id=>dispatch(fetchUser(id)))
 
+    // learning chain method from lodash
+
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id=>dispatch(fetchUser(id)))
+    .value();
 
 }
 
