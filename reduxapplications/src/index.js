ca5e67a22b8e8efad0535abcee62011d06bda597
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-18 15:34:10 +0530

    React Router DOM Dummy example

--- reduxapplications/src/index.js
+++ reduxapplications/src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+// import App from './components/App'
+import {BrowserRouter,Route} from 'react-router-dom'
+
+const pageOne = () =>
+{
+    return <div>Page One</div>
+}
+
+
+const pageTwo = () =>
+{
+    return( <div>Page Two
+    <button>Click me</button>
+    </div>)
+}
+
+const App = () =>
+{
+    return(
+        <div>
+            <BrowserRouter>
+                <div>
+                <Route path= "/" exact component = {pageOne} />
+                <Route path= "/pageTwo" exact component = {pageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+}
 
 
 ReactDOM.render(
@@ -13,6 +41,19 @@ ReactDOM.render(
 APP CHALLENGES
 
 1. Need to be able to navigate around
-to separate
+to separate pages in our app
+
+2. need to allow user to login/logout   
+
+3. NEED  to handle froms in redux
+
+4.need to master crud operation in react/redux
+
+5. good error handling
+-------------------------------------------------------
+
+
+
+
 
 */
