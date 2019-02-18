231dabb156b3a66af21eb085f500652e396a18a7
Shubham Gupta <shubhamg2595@gmail.com>
2019-02-18 16:23:58 +0530

    Stream App Router setup

--- reduxapplications/src/index.js
+++ reduxapplications/src/index.js
@@ -1,37 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App'
-import { BrowserRouter, Route ,Link} from 'react-router-dom'
+import App from './components/App'
 
-const pageOne = () => {
-    return (
-        <div>
-            <p>Page One</p>
-            <Link to='/pageTwo'>Navigate to page Two</Link>
-        </div>)
-}
-
-
-const pageTwo = () => {
-    return (<div><p>Page Two</p>
-
-        <Link to ='/'>Navigate to page One</Link>
-        <button>Click me</button>
-    </div>)
-}
-
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Route path="/" exact component={pageOne} />
-                    <Route path="/pageTwo" component={pageTwo} />
-                </div>
-            </BrowserRouter>
-        </div>
-    )
-}
 
 
 ReactDOM.render(
@@ -42,13 +12,10 @@ ReactDOM.render(
 
 /*
 
-//  <Route path= "/"  component = {pageOne} />
-// <Route path= "/pageTwo"  component = {pageTwo} />
-              
-if we dont use exact keyword, then above code will return b
-output of both the components , when we search for
-pageTwo component url...
+DIFFERENT ROUTER TYPES
 
-
+1. BROWSER ROUTER
+2.HASH ROUTER
+3. MEMORY ROUTER
 
 */
